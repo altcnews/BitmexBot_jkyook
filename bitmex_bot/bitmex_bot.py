e5577e46508881657764309da1ad30d011374ea8
jky <jinkwan.yook@gmail.com>
2018-09-08 22:14:54 +0900

    Merge branch 'jkyook' of https://github.com/jkyook/BitmexBot_jkyook into jkyook
    # Conflicts:
    #	bitmex_bot/bitmex_bot.py

--- bitmex_bot/bitmex_bot.py
+++ bitmex_bot/bitmex_bot.py
@@ -465,7 +465,7 @@ class OrderManager:
                                 self.stop_price = order['price'] - (order['price'] * settings.STOP_LOSS_FACTOR)
                             print("Order price {} \tStop Price {} \tProfit Price {} ".
                                   format(order['price'], self.stop_price, self.profit_price))
-                            # sleep(settings.API_REST_INTERVAL)
+                            sleep(settings.API_REST_INTERVAL)
                             # if settings.STOP_LOSS_FACTOR != "":
                             #     self.place_orders(side=self.SELL, orderType='StopLimit', quantity=self.amount,
                             #                       price=int(self.stop_price), stopPx=int(self.stop_price) - 5.0)
@@ -497,7 +497,7 @@ class OrderManager:
 
                             print("Order price {} \tStop Price {} \tProfit Price {} ".
                                   format(order['price'], self.stop_price, self.profit_price))
-                            # sleep(settings.API_REST_INTERVAL)
+                            sleep(settings.API_REST_INTERVAL)
                             # if settings.STOP_LOSS_FACTOR != "":
                             #     self.place_orders(side=self.BUY, orderType='StopLimit', quantity=self.amount,
                             #                       price=int(self.stop_price), stopPx=int(self.stop_price) - 5.0)
